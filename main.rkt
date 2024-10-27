793a403aaa70924bcb257ce0856788d0632446a1
alpaylan <alpkeles99@gmail.com>
2024-10-27 13:30:46 -0400

    move generation primitives to the repo

--- main.rkt
+++ main.rkt
@@ -1,4 +1,4 @@
 #lang racket
 
-(require "proplang.rkt" "shrinking.rkt")
-(provide (all-from-out "proplang.rkt" "shrinking.rkt"))
+(require "proplang.rkt" "shrinking.rkt" "generation.rkt")
+(provide (all-from-out "proplang.rkt" "shrinking.rkt" "generation.rkt"))
