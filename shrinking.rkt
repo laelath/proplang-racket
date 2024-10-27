ab59c7c0d05a870142d0aa219765004e39d57eb3
alpaylan <alpkeles99@gmail.com>
2024-10-27 12:07:09 -0400

    change the shrinker with a slightly more optimized version

--- shrinking.rkt
+++ shrinking.rkt
@@ -10,19 +10,20 @@
   [shrink-eager-retry (property? dict? dict? . -> . dict?)]))
 
 (define (shrink-eager prop shrinkers env)
-  (define (shrink-var env x shrinker)
-    (define v (dict-ref env x))
-    (let loop ([shrinks (shrinker v)])
-      (match shrinks
-        ['() v]
-        [(cons v vs)
-         (if (equal? (check-property prop (dict-set env x v)) 'fail)
-             (shrink-var (dict-set env x v) x shrinker)
-             (loop vs))])))
+  (define (shrink-var shrinkers env x)
+    (define shrinker (dict-ref shrinkers x))
+    (let down ([v (dict-ref env x)])
+      (let across ([shrinks (shrinker v)])
+        (match shrinks
+          ['() v]
+          [(cons v vs)
+           (if (equal? (check-property prop (dict-set env x v)) 'fail)
+               (down v)
+               (across vs))]))))
   (for/fold ([env env])
             ([x (property-variables prop)])
     (let ([v0 (dict-ref env x)]
-          [v1 (shrink-var env x (dict-ref shrinkers x))])
+          [v1 (shrink-var shrinkers env x)])
       (if (eq? v0 v1)
           env
           (dict-set env x v1)))))
